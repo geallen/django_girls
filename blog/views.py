3e03bc6114afe974fff0f45f86cf00e737d4e210
geallen <gamzesen3827@gmail.com>
2017-03-25 17:06:16 +0300

    Blog is created.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -6,3 +7,8 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
     return render(request, 'blog/post_list.html',  {'posts': posts})
+
+def post_details(request, pk):
+    #Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
